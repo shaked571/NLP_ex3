3da2f2dfca29b14606b2ee3f2edf7de550903b88
shaked571 <shakedgreenfeld@gmail.com>
2018-12-08 17:57:28 +0200

    git ignore

--- sol3.py
+++ sol3.py
@@ -3,6 +3,8 @@ import nltk
 import numpy as np
 
 # nltk.download()
+TAG = 'tag'
+WORD = 'word'
 parsed_sents = dependency_treebank.parsed_sents()  # Download all the data
 train_set = parsed_sents[:(int(len(parsed_sents) * 0.9))]
 test_set = parsed_sents[(int(len(parsed_sents) * 0.9)):]
@@ -16,16 +18,24 @@ first_sent = parsed_sents[1]
 # p1 = ("John")
 
 
-def feature_function(sentence):
+def feature_function(node1, node2, sentence):
     sentence_vector = {}
     for node_i in sentence.nodes:
         for node_j in sentence.nodes:
             if node_i is not node_j:
-                sentence_vector[(sentence.nodes[node_i]['word'], sentence.nodes[node_j]['word'])] = 1
-                sentence_vector[(sentence.nodes[node_i]['tag'], sentence.nodes[node_j]['tag'])] = 1
-
+                if sentence.nodes[node_i][WORD] is node1[WORD] and sentence.nodes[node_j][WORD] is node2[WORD]:
+                    sentence_vector[(sentence.nodes[node_i][WORD], sentence.nodes[node_j][WORD])] = 1
+                else:
+                    sentence_vector[(sentence.nodes[node_i][WORD], sentence.nodes[node_j][WORD])] = 0
+                if sentence.nodes[node_i][TAG] is node1[TAG] and sentence.nodes[node_j][TAG] is node2[TAG]:
+                    sentence_vector[(sentence.nodes[node_i][TAG], sentence.nodes[node_j][TAG])] = 1
+                else:
+                    sentence_vector[(sentence.nodes[node_i][TAG], sentence.nodes[node_j][TAG])] = 0
     return sentence_vector
-ret = feature_function(first_sent)
+
+n1 = first_sent.nodes[0]
+n2 = first_sent.nodes[1]
+ret = feature_function(n1, n2, first_sent)
 for kv in ret:
     print(kv)
 
