fb4cc35965aa39071e3221359536b0ecca609cd8
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2018-12-08 14:35:20 +0200

    a completed

--- sol3.py
+++ sol3.py
@@ -1,7 +1,13 @@
 from nltk.corpus import dependency_treebank
 import nltk
 
-nltk.download()
+# nltk.download()
 parsed_sents = dependency_treebank.parsed_sents()  # Download all the data
-print("hi")
+train_set = parsed_sents[:(int(len(parsed_sents) * 0.9))]
+test_set = parsed_sents[(int(len(parsed_sents) * 0.9)):]
+
+
+# first_sent = parsed_sents[1]
+# for node in first_sent.nodes:
+#     print(first_sent.nodes[node]['word'])
 
